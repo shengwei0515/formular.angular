b24a795e935cadc4c46f7c91b9286144d2dc498e
shengwei0515 <shengwei199505@gmail.com>
2021-11-23 02:26:14 +0800

    feat: add button and fix type bug

--- formular/src/app/app.module.ts
+++ formular/src/app/app.module.ts
@@ -4,17 +4,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SettingComponent } from './pages/setting/setting.component';
 import { VmTableComponent } from './pages/vm-table/vm-table.component';
 
+import { AccordionModule } from 'primeng/accordion';
+import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
 import { DynamicDialogModule } from 'primeng/dynamicdialog';
+import { InputTextModule } from 'primeng/inputtext';
 import { MessageModule } from 'primeng/message';
 import { MessagesModule } from 'primeng/messages';
-import { AccordionModule } from 'primeng/accordion';
+
 
 import { AppConfigService, initConfig } from './core/services/app-config/app-config.service';
 import { DetialCardComponent } from './pages/vm-table/detial-card/detial-card.component';
@@ -25,22 +27,24 @@ import { AzureSpSettingCardComponent } from './pages/setting/azure-sp-setting-ca
 @NgModule({
   declarations: [
     AppComponent,
-    SettingComponent,
-    VmTableComponent,
+    AzureSpSettingCardComponent,
     DetialCardComponent,
-    AzureSpSettingCardComponent
+    SettingComponent,
+    VmTableComponent
   ],
   imports: [
+    AppRoutingModule,
+    AccordionModule,
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule,
-    NoopAnimationsModule,
-    AppRoutingModule,
-    TableModule,
+    ButtonModule,
     DynamicDialogModule,
-    MessageModule,
+    HttpClientModule,
+    InputTextModule,
     MessagesModule,
-    AccordionModule
+    MessageModule,
+    NoopAnimationsModule,
+    TableModule
   ],
   providers: [
     AppConfigService,
